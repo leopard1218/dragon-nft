15c99645d12817e8ec472ed188625f3d882d512d
leopard1218 <snowleopards1218@gmail.com>
2021-12-18 10:11:11 +0900

    commit 5

--- Backend/script.js
+++ Backend/script.js
@@ -3,12 +3,13 @@ const fs = require('fs');
 const path = require('path');
 const Canvas = require('canvas');
 
-const head = ['green', 'red', 'blue', 'pink', 'purple', 'grey', 'aa', 'bb', 'cc', 'dd'];
-const horn = ['green', 'red', 'blue', 'pink', 'purple', 'grey', 'aa', 'bb', 'cc', 'dd'];
-const jeans = ['green', 'red', 'blue', 'pink', 'purple', 'grey', 'aa', 'bb', 'cc', 'dd'];
-const shirt = ['green', 'red', 'blue', 'pink', 'purple', 'grey', 'aa', 'bb', 'cc', 'dd'];
-const shoes = ['green', 'red', 'blue', 'pink', 'purple', 'grey', 'aa', 'bb', 'cc', 'dd'];
-const tail = ['green', 'red', 'blue', 'pink', 'purple', 'grey', 'aa', 'bb', 'cc', 'dd'];
+const background = ['darkgreen', 'gray', 'green', 'green-white', 'green-yellow', 'purple', 'red', 'red-yellow', 'violet', 'yellow'];
+const head = ['blue', 'gray-red-eye', 'green', 'green-red-eye', 'green-yellow-eye', 'orange', 'pink', 'red', 'violet', 'yellow'];
+const horn = ['blue-horn', 'brown-horn', 'brown-horn-cap', 'cyan-cap', 'green-horn', 'orange-horn', 'pink-horn', 'purple-cap', 'purple-crown-cap', 'red-horn'];
+const jeans = ['blue', 'brown', 'cyan', 'dark-green', 'gray', 'green-red', 'orange', 'pink', 'purple', 'red'];
+const shirt = ['blue-mark', 'darkblue-mark', 'darkgray-mark', 'gray-mark', 'green-dot', 'orange-mark', 'pink-dot', 'pink-mark', 'red-mark', 'yellow-dot'];
+const shoes = ['cyan', 'darkpurple', 'gray', 'green', 'green-red', 'pink', 'purple', 'red-gray', 'red-yellow', 'yellow'];
+const tail = ['blue-pink', 'darkpurple', 'darkpurple-cyan', 'green-yellow', 'purple-green', 'purple-pink', 'purple-red', 'red-cyan', 'red-green', 'red-pink'];
 // const character = [
 //   ['red', 'pink', 'green', 'red', 'blue', 'green']
 // ]
@@ -17,41 +18,45 @@ const character = [];
 let children = [];
 
 const randomFunc = () => {
-  return Math.trunc(Math.random() * 10 ** 6)
+  return Math.trunc(Math.random() * 10 ** 7)
 }
 const generateCharacter = () => {
   let random = randomFunc()
-  let repeatState = 0;
-  for(let j = 0; j < repeatation.length; j++) {
-    if(repeatation[j] === random) {
-      repeatState = 1;
-    }
-  }
-  if(repeatState === 0) {
-    return random;
-  } else {
-    console.log('repeatation')
-    generateCharacter()
-  }
+  return random;
+  // let repeatState = 0;
+  // for(let j = 0; j < repeatation.length; j++) {
+  //   if(repeatation[j] === random) {
+  //     repeatState = 1;
+  //   }
+  // }
+  // if(repeatState === 0) {
+  //   repeatation.push(random)
+  //   return random;
+  // } else {
+  //   console.log('repeatation')
+  //   generateCharacter()
+  // }
 
 }
 
 const generateAll = () => {
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 1000; i++) {
     let value = generateCharacter().toString();
-    if(value.length < 6) {
-      let zero = 6 - value.length;
+    // console.log(value)
+    if(value.length < 7) {
+      let zero = 7 - value.length;
       for(let k = 0; k < zero; k++) {
         value = 0 + value;
       }
     }
     children = []
-    children.push(tail[value.slice(0, 1)])
-    children.push(head[value.slice(1, 2)])
-    children.push(horn[value.slice(2, 3)])
-    children.push(jeans[value.slice(3, 4)])
-    children.push(shirt[value.slice(4, 5)])
-    children.push(shoes[value.slice(5, 6)])
+    children.push(background[value.slice(0, 1)])
+    children.push(tail[value.slice(1, 2)])
+    children.push(head[value.slice(2, 3)])
+    children.push(horn[value.slice(3, 4)])
+    children.push(jeans[value.slice(4, 5)])
+    children.push(shirt[value.slice(5, 6)])
+    children.push(shoes[value.slice(6, 7)])
     character.push(children)
   }
   
@@ -63,17 +68,35 @@ const prefix = path.resolve('public/');
 let i;
 character.forEach((n, idx) => {
   i = 0;
-  let imgs = n.map(attr => {
-    i += 1;
-    if(i == 1) return prefix + "/tail/" + attr + '.PNG'
-    else if(i == 2) return prefix + "/head/" + attr + '.PNG'
-    else if(i == 3) return prefix + "/horn/" + attr + '.PNG'
-    else if(i == 4) return prefix + "/jeans/" + attr + '.PNG'
-    else if(i == 5) return prefix + "/shirt/" + attr + '.PNG'
-    else if(i == 6) return prefix + "/shoes/" + attr + '.PNG'    
+  let imgs = n.map(attr => {    
+    i += 1; 
+    if(i == 1) return prefix + "/background/" + attr + '.PNG'
+    else if(i == 2) return prefix + "/tail/" + attr + '.PNG'
+    else if(i == 3) return prefix + "/head/" + attr + '.PNG'
+    else if(i == 4) return prefix + "/horn/" + attr + '.PNG'
+    else if(i == 5) return prefix + "/jeans/" + attr + '.PNG'
+    else if(i == 6) return prefix + "/shirt/" + attr + '.PNG'
+    else if(i == 7) return prefix + "/shoes/" + attr + '.PNG' 
+      
   });
-  let imagePath = path.resolve('image') + `/${idx + 1}.png`;
-  let metadataPath = path.resolve('metadata') + `/${idx + 1}.json`
+  let imagePath = path.resolve('image') + `/${idx + 1 + 9000}.png`;
+  let metadataPath = path.resolve('metadata') + `/${idx + 1 + 9000}.json`;
+  const metadata = {
+    description: "Dragon is a collection of Crypto Dragon.",
+    name: "Betting dragon #" + (idx + 1 + 9000),
+    attributes: {
+      background: character[idx][0],
+      head: character[idx][2],
+      horncap: character[idx][3],
+      jeans: character[idx][4],
+      shirt: character[idx][5],
+      shoes: character[idx][6],
+      tail: character[idx][1]    
+    },
+    image: "https://gateway.pinata.cloud/ipfs/QmehouJ1vDfbqTtHRhef76Yr4pBPfk7mwU8FrtjsQL56hm/1.jpg"
+  }
+  // console.log(imgs)
+  fs.writeFileSync(metadataPath, JSON.stringify(metadata))
   mergeImages(imgs, {
     Canvas: Canvas
   }).then(img => {
